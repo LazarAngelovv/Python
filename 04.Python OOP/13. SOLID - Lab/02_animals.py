f6f6b211aa7a8f80bba9429771f63cd3cba0abf3
Lazar Angelov <79146125+LazarAngelovv@users.noreply.github.com>
2022-05-03 20:41:59 +0300

    Add files via upload

---
+++ 04.Python OOP/13. SOLID - Lab/02_animals.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+
+class Cat(Animal):
+    def make_sound(self):
+        return "meow"
+
+
+class Dog(Animal):
+    def make_sound(self):
+        return "woof-woof"
+
+
+class Chicken(Animal):
+    def make_sound(self):
+        return "chick-chirick"
+
+
+def animal_sound(animals: list):
+    for animal in animals:
+        print(animal.make_sound())
+
+
+animals = [Cat(), Dog(), Chicken()]
